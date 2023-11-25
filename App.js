40594c530196b754267c689eec255989a18b91c3
marvinbra <marvinbopp37@gmail.com>
2023-11-25 17:37:49 +0100

    app.js + index.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Image from "./components/Image"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+export default function App() {
+    return (
+        <div>
+            <Image />
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    )
+}
